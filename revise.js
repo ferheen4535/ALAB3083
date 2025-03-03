93cee90bd83e3e47b419e65e108cfd30fa94303a
ferheen4535 <ferheen.syed@gmail.com>
2025-03-03 07:38:17 -0500

    revise

---
+++ revise.js
@@ -0,0 +1,22 @@
+//for loops = repeat some code a limited of times//
+// i is a variable used as a counter//
+
+for(let i = 0;  i <= 2; i++){      // i = how many iterations, conditions is continue loops until i is equal or less then 2 (0,1,2) so 3 times; 
+                                    // and increase one time//
+    console.log("i")
+}
+
+for(let i = 0;  i < 10; i++){      // i = how many iterations, conditions is continue loops until i is equal or less then 10 (0,1,2) so 11 times; 
+                                    // and increase one time//
+    console.log("j")
+}
+
+for(let i = 2;  i <= 10; i+= 2){      // i = how many iterations; conditions is continue loops until i is equal or less then 2 (0,1,2) so 3 times; 
+                                    // and increase by 2s //
+    console.log("m")
+}
+
+for(let i = 2;  i <= 10; i--){      // i = how many iterations, conditions is continue loops until i is equal or less then 2 (0,1,2) so 3 times; 
+    // and increase by 2s //
+console.log("z")
+}
